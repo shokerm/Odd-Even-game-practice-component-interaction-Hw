43049011f36c8afa82896540c86e526eb73b93e5
shokerm <96984377+shokerm@users.noreply.github.com>
2022-10-19 12:01:45 +0300

    Update app.component.ts

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EvenOddNubmersService } from './even-odd-nubmers.service';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
   randomNUmber = 0;
   
@@ -14,9 +17,25 @@ export class AppComponent {
  
 
   selectNumber(value: number){
+    //A.
+    // this.randomNUmber = value;
+    // value % 2 ===0 ? this.evenArray.push(value): this.oddArray.push(value);
+    // console.log(this.evenArray);
+    // console.log(this.oddArray);
+   //B.
     this.randomNUmber = value;
-    value % 2 ===0 ? this.evenArray.push(value): this.oddArray.push(value);
+    value % 2 ===0 ? this.EvenOddNumbersService.evenArray.push(value): this.EvenOddNumbersService.oddArray.push(value);
     console.log(this.evenArray);
     console.log(this.oddArray);
   }
+
+  constructor(private EvenOddNumbersService:EvenOddNubmersService ){
+
+  }
+
+ ngOnInit():void {
+  this.evenArray = this.EvenOddNumbersService.evenArray;
+  this.oddArray = this.EvenOddNumbersService.oddArray;
+
+ }
 }
